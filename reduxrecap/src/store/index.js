29ad41ebd2967687a81c9896f344e8cf1e92eeb9
notharsha007 <notharsha.007@gmail.com>
2024-10-29 11:18:30 +0530

    done some minor changes

--- reduxrecap/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import uiSlice from './ui-slice.js';
-import cartSlice from "./cart-slice.js";
-
-const store= configureStore({
-    reducer :{ui:uiSlice.reducer, cart: cartSlice.reducer},
-});
-
-export default store;
